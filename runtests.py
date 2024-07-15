9f3c5806c3c5098deeba2858768b1d8d236e5ab2
James Adams <james.adams@stfc.ac.uk>
2024-07-15 15:33:48 +0100

    Fix whitepace around brackets

--- runtests.py
+++ runtests.py
@@ -125,7 +125,7 @@ class Testlinksync(unittest.TestCase):
         """return a list of (linkname, linktarget) tuples for all files in a directory"""
         pj = os.path.join
         readlink = os.readlink
-        result = [ (l, readlink(pj(directory, l))) for l in os.listdir(directory) ]
+        result = [(l, readlink(pj(directory, l))) for l in os.listdir(directory)]
         result.sort()
         return result
 
